c840a2fbdf062ec0655a4d11301937e1a434759e
dhirengshetty14 <dhirenedu@gmail.com>
2026-03-31 20:00:15 -0400

    Added ApiClient

--- app/src/main/java/com/projs/ecommerceshopping/model/ApiClient.kt
+++ app/src/main/java/com/projs/ecommerceshopping/model/ApiClient.kt
@@ -1,6 +1,32 @@
 package com.projs.ecommerceshopping.model
 
-object ApiClient {
-    private val BASE_URL="http://103.163.198.93/myshop/index.php/"
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+object ApiClient {
+
+    private const val BASE_URL = "http://103.163.198.93/myshop/index.php/"
+
+    private val retrofit by lazy {
+
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val apiService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
 }
